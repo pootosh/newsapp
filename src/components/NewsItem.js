772082b2f9e83e2da363acba9b47b906ce8172ff
Aditya Kumar SIngh <adityasingh9501@gmail.com>
2022-07-30 21:41:48 +0530

    News Item using SampleOutput.json

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,18 +3,18 @@ import React, { Component } from "react";
 export class NewsItem extends Component {
   render() {
 
-    let {title, description} = this.props;
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
       <div>
-        <div className="card" style={{width: "18rem"}}>
-          <img src="..." className="card-img-top" alt="..." />
+        <div className="card my-3" style={{width: "18rem"}}>
+          <img src={imageUrl} className="card-img-top" alt="..." style={{height: "150px"}} />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
+            <h5 className="card-title" style={{height: "80px"}}>{title.slice(0, 50)}...</h5>
             <p className="card-text">
-              {description}
+              {description.slice(0, 100)}...
             </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
+            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">
+              Read Mode
             </a>
           </div>
         </div>
